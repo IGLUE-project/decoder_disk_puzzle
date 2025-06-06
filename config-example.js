7f75099adce8587803a42b688baeb324dd909f23
Javier Ferreño <javifc67@gmail.com>
2025-06-06 11:30:33 +0200

    creado un config example para desarrollo

---
+++ config-example.js
@@ -0,0 +1,47 @@
+//Copy this file to config.js and specify your own settings
+
+export let ESCAPP_APP_SETTINGS = {
+  //Settings that can be specified by the authors
+  skin: "FUTURISTIC", //skin can be "STANDARD", "RETRO" or "FUTURISTIC" or "BASIC".
+  //background: "NONE", //background can be "NONE" or a URL.
+  actionAfterSolve: "SHOW_MESSAGE", //actionAfterSolve can be "NONE" or "SHOW_MESSAGE".
+  //message: "Custom message",
+
+  //type: can be "LETTERS", "NUMBERS", "SHAPES", "COLORED SHAPES", "COLORS", "CUSTOM".
+  //areaColor: can be a color or "RAINBOW" for a rainbow gradient.
+  //length: number of slices in the wheel.
+  numberOfWheels: 3, //number of wheels in the puzzle.
+  wheelsType: [
+    {
+      type: "NUMBERS",
+      length: 6,
+      areaColor: "RAINBOW",
+    },
+    {
+      type: "COLORED SHAPES",
+      length: 6,
+    },
+    {
+      type: "CUSTOM",
+      length: 6,
+      customWheel: [
+        { areaColor: "#ff0000", colorIco: "brown", label: "a", img: "", ico: "circle" },
+        { areaColor: "#ffff00", colorIco: "red", label: "b", img: "", ico: "triangle" },
+        { areaColor: "#ff00ff", colorIco: "blue", label: "c", img: "", ico: "square" },
+        { areaColor: "#00ff00", colorIco: "green", label: "d", img: "", ico: "pentagon" },
+        { areaColor: "#00ffff", colorIco: "yellow", label: "e", img: "", ico: "star" },
+        { areaColor: "#ff0000", colorIco: "orange", label: "g", img: "", ico: "asdasd" },
+      ],
+    },
+  ],
+
+  //Settings that will be automatically specified by the Escapp server
+  solutionLength: 3,
+  locale: "es",
+
+  escappClientSettings: {
+    endpoint: "https://escapp.etsisi.upm.es/api/escapeRooms/153",
+    linkedPuzzleIds: [4],
+    rtc: false,
+  },
+};
